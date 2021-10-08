82fafbd4239b730c804ea9f742168d140aaeaf75
kushgupta-official <kushgupta.potus@gmail.com>
2021-10-09 03:44:00 +0530

    Deployment Bug

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ window.onload=function(){
                 debug: false
             // }
         },
-        scene:[play]
+        scene:[Play]
     }
     game=new Phaser.Game(config);
     window.focus();
